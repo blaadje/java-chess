e007c1eb2de36cc220f2bd031446589bc5d9bee3
Alex <acharlot91@gmail.com>
2018-02-01 12:28:17 +0100

    adding plateau

--- src/main.java
+++ src/main.java
@@ -6,7 +6,21 @@ public class main {
     }
 }
 
-class Case
+class Plateau
 {
+    public static int SIZE = 8;
 
+    private Case[][] Plateau = new Case[SIZE][SIZE];
+
+    public Plateau () {
+        for (int i = 0; i < Plateau.length; i++) {
+            for (int j = 0; j < Plateau.length; j++) {
+                this.Plateau[i][j] = new Case(i, j);
+            }
+        }
+    }
+
+    public Case getCase (int x, int y) {
+        return Case[x][y];
+    }
 }
